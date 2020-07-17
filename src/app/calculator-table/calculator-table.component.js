5980ec19a93a781f730127992dacc58ae710552e
Michael Vasquez <michael.vasquez@vertexinc.com>
2020-07-17 18:35:21 -0400

    Selecting and Unselecting Rows and getting Avg

--- src/app/calculator-table/calculator-table.component.js
+++ src/app/calculator-table/calculator-table.component.js
@@ -10,6 +10,9 @@ const getUnit = (data) => {
 
 function CalculatorTableController(Database) {
     this.data = Database.getData();
+    this.selectedRecords = [];
+    this.hasSelectedRows = false;
+    this.selectedRowsAverage = 0;
 
     if (!this.data || !this.data.records) {
         this.noDataOrRecords = true;
@@ -18,6 +21,8 @@ function CalculatorTableController(Database) {
 
     const records = this.data.records;
 
+
+    /** For Last 7 Average **/
     let sortedData = records.sort((a, b) => (new Date(b.date)) - (new Date(a.date))).slice();
 
     const lastSeven = [];
@@ -34,9 +39,44 @@ function CalculatorTableController(Database) {
     this.records = records;
     this.unit = getUnit(this.data);
     this.average = Math.round((average + Number.EPSILON) * 100) / 100
+
+    /** Selection Handlers **/
+
+    const updateSelectedRowsAvg = () => {
+        if (this.selectedRecords.length < 1) {
+            this.selectedRowsAverage = 0;
+            return;
+        }
+        let total = this.selectedRecords.reduce((total, record) => parseFloat(record.value) + total, 0.0);
+        let average = total / this.selectedRecords.length;
+        this.selectedRowsAverage = Math.round((average + Number.EPSILON) * 100) / 100;
+    };
+
+    this.isSelected = (rowElement) => this.selectedRecords.indexOf(rowElement.record) > -1;
+
+    this.toggleRowSelected = (rowElement) => {
+        const indexOfRecord = this.selectedRecords.indexOf(rowElement.record);
+
+        if (indexOfRecord > -1) {
+            this.selectedRecords.splice(indexOfRecord, 1);
+        } else {
+            this.selectedRecords.push(rowElement.record);
+        }
+
+        this.hasSelectedRows = this.selectedRecords.length > 0;
+        updateSelectedRowsAvg();
+    };
+
+    this.handleDeleteClick = () => {};
+    this.handleUnselectClick = () => {
+        this.selectedRecords.splice(0, this.selectedRecords.length);
+        this.hasSelectedRows = false;
+        updateSelectedRowsAvg();
+    };
 };
 
 CalculatorTableController.$inject = ['Database'];
+
 
 CalculatorTableModule.component(CALCULATOR_TABLE_MODULE_NAME, {
     template,
